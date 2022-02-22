290b18dc6514d9a543a83ed88bd5ed63079ce4c8
Sam Smith <samlsmith424@gmail.com>
2022-02-22 09:05:21 -0800

    User story 1

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -18,6 +18,21 @@ RSpec.describe 'studio index page' do
   end
 
   it 'shows the titles of all its movies' do
+    studio_1 = Studio.create!(name: "Hollywood Movies", location: "Los Angeles")
+    studio_2 = Studio.create!(name: "We Love Movies", location: "Orange County")
+    studio_3 = Studio.create!(name: "Pixar", location: "SoCal")
 
+    movie_1 = studio_1.movies.create!(title: "V for Vendetta", creation_year: 2005, genre: "Action")
+    movie_2 = studio_1.movies.create!(title: "Hercules", creation_year: 1997, genre: "Action")
+    movie_3 = studio_1.movies.create!(title: "Perfect Storm", creation_year: 2000, genre: "Drama")
+
+    movie_4 = studio_2.movies.create!(title: "Saving Private Ryan", creation_year: 1998, genre: "Action")
+    movie_5 = studio_2.movies.create!(title: "Shutter Island", creation_year: 2010, genre: "Thriller")
+
+    movie_6 = studio_3.movies.create!(title: "Inception", creation_year: 2010, genre: "Suspense")
+
+    visit "/studios"
+
+    expect(page).to have_content(movie_1.title)
   end
 end
